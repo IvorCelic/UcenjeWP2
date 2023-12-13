dfdf7a17bedf6dc7013f2298628fc9842cab10ee
Ivor Ćelić <ivorcelic@gmail.com>
2023-12-14 00:59:10 +0100

    WebApiDemo

---
+++ CS/WebAPI/Controllers/Test.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class Test : Controller
+    {
+        [HttpGet]
+        [Route("Hello")]
+        public string Hello(string PrviBroj, string DrugiBroj)
+        {
+            return Console.ReadLine PrviBroj + DrugiBroj;
+        }
+    }
+}
---
+++ CS/WebAPIDemo/Controllers/BooksController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIDemo.Controllers
+{
+    [ApiController]
+    public class BooksController: ControllerBase
+    {
+        [HttpGet]
+        [Route("/books")]
+        public string GetBooks()
+        {
+            return "izlistavam sve knjige.";
+        }
+
+        [HttpGet]
+        [Route("/books/{ID}")]
+        public string GetBookByID(int ID)
+        {
+            return $"Iščitavam knjigu: {ID}";
+        }
+
+        [HttpPost]
+        [Route("/books")]
+        public string CreateBook() 
+        {
+            return $"Kreiram knjigu.";
+        }
+
+        [HttpPut]
+        [Route("/books/{ID}")]
+        public string UpdateBook(int ID)
+        {
+            return $"Ažuriram knjigu: {ID}";
+        }
+
+        [HttpDelete]
+        [Route("/books/{ID}")]
+        public string DeleteBook(int ID)
+        {
+            return $"Brišem knjigu: {ID}";
+        }
+    }
+}
