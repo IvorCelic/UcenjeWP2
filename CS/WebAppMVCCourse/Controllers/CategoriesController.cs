6a386dcf437e28a5ce976c4768f78d2dc1ec982c
Ivor Ćelić <ivorcelic@gmail.com>
2024-01-08 18:59:36 +0100

    crud

--- CS/WebAppMVCCourse/Controllers/CategoriesController.cs
+++ CS/WebAppMVCCourse/Controllers/CategoriesController.cs
@@ -32,5 +32,30 @@ namespace WebAppMVCCourse.Controllers
             return View(category);
         }
 
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.AddCategory(category);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int categoryID)
+        {
+            CategoriesRepository.DeleteCategory(categoryID);
+            
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
