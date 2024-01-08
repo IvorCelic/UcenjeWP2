1edadba77709951b96dfe2e8ca23a301a40da35c
Ivor Ćelić <ivorcelic@gmail.com>
2024-01-08 13:59:43 +0100

    osnovna web aplikacija sa MVC strukturom

---
+++ CS/WebAppMVCCourse/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppMVCCourse.Models;
+
+namespace WebAppMVCCourse.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = CategoriesRepository.GetCategories();
+
+            return View(categories);
+        }
+
+        public IActionResult Edit(int? ID)
+        {
+            var category = CategoriesRepository.GetCategoryByID(ID.HasValue ? ID.Value : 0);
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.UpdateCategory(category.CategoryID, category);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+    }
+}
---
+++ CS/WebAppMVCCourse/Models/CategoriesRepository.cs
@@ -0,0 +1,57 @@
+﻿namespace WebAppMVCCourse.Models
+{
+    public static class CategoriesRepository
+    {
+        private static List<Category> _categories = new List<Category>()
+        {
+            new Category {CategoryID = 1, Name = "Piće", Description = "Piće"},
+            new Category {CategoryID = 2, Name = "Slastice", Description = "Slastice"},
+            new Category {CategoryID = 3, Name = "Meso", Description = "Meso"}
+        };
+
+        public static void AddCategory(Category category)
+        {
+            var maxID = _categories.Max(x => x.CategoryID);
+            category.CategoryID = maxID + 1;
+            _categories.Add(category);  
+        }
+        public static List<Category> GetCategories() => _categories;
+
+        public static Category? GetCategoryByID(int categoryID)
+        {
+            var category = _categories.FirstOrDefault(x => x.CategoryID == categoryID);
+            if (category != null)
+            {
+                return new Category
+                {
+                    CategoryID = category.CategoryID,
+                    Name = category.Name,
+                    Description = category.Description,
+                };
+            }
+
+            return null;
+        }
+
+        public static void UpdateCategory(int categoryID, Category category)
+        {
+            if (categoryID != category.CategoryID) return;
+
+            var categoryToUpdate = _categories.FirstOrDefault(x => x.CategoryID == categoryID);
+            if (categoryToUpdate != null )
+            {
+                categoryToUpdate.Name = category.Name;
+                categoryToUpdate.Description = category.Description;
+            }
+        }
+
+        public static void DeleteCategory(int categoryID)
+        {
+            var category = _categories.FirstOrDefault(x => x.CategoryID == categoryID);
+            if (category != null )
+            {
+                _categories.Remove(category);
+            }
+        }
+    }
+}
