e1365e471da3fef0f7c56df192b5b22fd79c621f
Ivor Ćelić <ivorcelic@gmail.com>
2024-01-20 17:53:00 +0100

    Ljubavni Kalkulator - ostalo još izračunavanje riješiti do kraja

--- CS/UcenjeCS/V04LjubavniKalkulator/LjubavniKalkulator.cs
+++ CS/UcenjeCS/V04LjubavniKalkulator/LjubavniKalkulator.cs
@@ -1,4 +1,6 @@
-﻿namespace UcenjeCS.V04LjubavniKalkulator
+﻿
+
+namespace UcenjeCS.V04LjubavniKalkulator
 {
 
     // Upiši dva imena, potom ispod svakog slova ispiši koliko se puta ono ponavlja.
@@ -21,5 +23,62 @@
             DrugoIme = drugoIme;
         }
 
+        public string Rezultat()
+        {
+            return Izracunaj(SlovaNiz(PrvoIme+DrugoIme)) + " %";
+        }
+
+        private int Izracunaj(int[] brojevi)
+        {
+            while (brojevi.Length >= 3)
+            {
+                int[] zbrojevi = new int[(brojevi.Length + 1) / 2];
+
+                for (int i = 0; i < (brojevi.Length + 1) / 2; i++)
+                {
+                    if (i == (brojevi.Length - 1) / 2 && brojevi.Length % 2 != 0)
+                    {
+                        zbrojevi[i] = brojevi[i];
+                    }
+                    else
+                    {
+                        zbrojevi[i] = brojevi[i] + brojevi[brojevi.Length - 1 - i];
+                    }
+
+                }
+
+                Console.WriteLine(string.Join(",", zbrojevi));
+            }
+
+
+
+            return 56;
+        }
+        private int[] SlovaNiz(string imena)
+        {
+            int Ukupno;
+            int Index = 0;
+            imena = imena.Replace(" ", "").ToLower();
+
+            int[] brojevi = new int[imena.Length];
+
+            foreach (char c in imena)
+            {
+                Ukupno = 0;
+                foreach (char cc in imena)
+                {
+                    if (c == cc)
+                    {
+                        Ukupno++;
+                    }
+                }
+                brojevi[Index++] = Ukupno;
+            }
+
+            Console.WriteLine(imena);
+            Console.WriteLine(string.Join(",", brojevi));
+
+            return brojevi;
+        }
     }
 }
--- CS/UcenjeCS/V04LjubavniKalkulator/Program.cs
+++ CS/UcenjeCS/V04LjubavniKalkulator/Program.cs
@@ -4,67 +4,33 @@
     {
         public static void Izvedi()
         {
-            Ljubav();
-        }
-
-        private static void Ljubav()
-        {
-            LjubavniKalkulator ljubavniKalkulator = new();
-
-            ljubavniKalkulator.PrvoIme = Unos("Unesite prvo ime: ");
-            ljubavniKalkulator.DrugoIme = Unos("Unesite drugo ime: ");
-
-            int[] prvoImePrebroj = PrebrojSlova(ljubavniKalkulator.PrvoIme);
-            int[] drugoImePrebroj = PrebrojSlova(ljubavniKalkulator.DrugoIme);
-
-            Console.WriteLine(prvoImePrebroj);
-            Console.WriteLine(drugoImePrebroj);
+            LjubavniZov();
         }
 
         private static string Unos(string poruka)
         {
-            int[] Slova = new int[Unos(poruka).Length];
-            int Index = 0;
-            int Ukupno;
-            string unos;
+            string Unos;
 
             while (true)
             {
-                Console.Write(poruka);
-                unos = Console.ReadLine();
 
-                if (unos.Trim().Length == 0)
+                Console.Write(poruka);
+                Unos = Console.ReadLine();
+
+                if (Unos.Trim().Length == 0)
                 {
-                    Console.WriteLine("Morate unijeti ime!");
+                    Console.WriteLine("Unos obavezan.");
                     continue;
                 }
 
-                return string.Join(",", Slova);
+
+
+                return Unos;
             }
-
         }
-
-        private static int[] PrebrojSlova(string unos)
+        private static void LjubavniZov()
         {
-            int[] prebrojSlova = new int[unos.Length];
-
-            for (int i = 0; i < unos.Length; i++)
-            {
-                int Ukupno = 0;
-                foreach (char c in unos)
-                {
-                    if (unos[i] == c)
-                    {
-                        Ukupno++;
-                    }
-                }
-
-                prebrojSlova[i] = Ukupno;
-            }
-
-            return prebrojSlova;
+            Console.WriteLine(new LjubavniKalkulator(Unos("Prvo ime: "), Unos("Drugo ime: ")).Rezultat());
         }
-
-
     }
 }
